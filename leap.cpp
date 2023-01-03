6fa94bf5ad587556caa25d0595c0cb84d200a024
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 22:58:32 +0530

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -5,6 +5,16 @@ int main()
     int year,k=0;
     cout<<"Enter the Year:";
     cin>>year;
-    if
+    if(year%400==0)
+        k=1;
+    else if(year%100==0)
+        k=0;
+    else if(k%4==0)
+        k=1;
+    if(k==1)
+        cout<<"The yesr is a Leap Year."<<"\n";
+        else 
+            cout<<"The year is not a Leap Year."<<"\n";
+}
     
 
