54c5ba146e733aa2e281e50401caf7b0d363e44e
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 23:53:22 +0530

    Create ncr.cpp

---
+++ ncr.cpp
@@ -0,0 +1,35 @@
+#include<iostream> 
+  
+ using namespace std; 
+ int fact(int n) 
+ { 
+         int f=1; 
+         while(n>0) 
+         { 
+                 f=f*n; 
+                 n=n-1; 
+         } 
+         return (f); 
+ } 
+ int main() 
+ { 
+         int n,r,s; 
+         cout<<"Enter the Values of n and r:"; 
+         cin>>n; 
+         cin>>r; 
+         if(n==0)
+         { 
+                 s=0; 
+                 cout<<"Value of nCr="<<s; 
+         }
+         else if (n>=r) 
+         { 
+                 int a=fact(n); 
+                 int b=fact(r); 
+                 int c=fact(n-r); 
+                 s=a/(b*c); 
+                 cout<<"Value of nCr="<<s; 
+          } 
+         else cout<<"Invalid Input"; 
+         return(0); 
+ }
