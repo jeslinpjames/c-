06308ccd023676c2566fe47a2e2a46f1c6d1664f
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 23:41:19 +0530

    Update quad.cpp

--- quad.cpp
+++ quad.cpp
@@ -1,42 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main()
 {
-     int a,b,c,r1,r2,d,k=0,real, imaginary;
-     cout<<"Enter the values of a b and c:";
-     cin>>a;
-     cin>>b;
-     cin>>c;
-     if(a!=0)
-     {
-          d=(b*b)-(4*a*c);
-          if(d>0)
-          {
-              r1=(b*-1)+sqrt(d)/(2*a);
-              r2=(b*-1)-sqrt(d)/(2*a);
-              k=1;
-          }
-          else if(d==0)
-          {
-              r1=(b*-1)/(2*a);
-              r2=r1;
-              k=1;
-          }
-          else if(d<0)
-          real =(b*-1)/(2*a);
-          imaginary=(sqrt(d))/(2*a);
-          cout<<real<<"+"<<imaginary<<"i"<<"\n";
-          cout<<real<<"-"<<imaginary<<"i"<<"\n";
-          k=0;
-          if(k==1)
-          cout<<r1<<" and "<<r2<<" are the roots to the equation";
-          else 
-          cout<<"Invalid Input ";
-     }
+    double a, b, c;
+    cout << "Enter the coefficients a, b, and c: ";
+    cin >> a >> b >> c;
 
+    double d = b*b - 4*a*c;
+
+    if (d > 0) {
+        double root1 = (-b + sqrt(d)) / (2*a);
+        double root2 = (-b - sqrt(d)) / (2*a);
+        cout << "Root 1: " << root1 << endl;
+        cout << "Root 2: " << root2 << endl;
+    }
+    else if (d == 0) {
+        double root = -b / (2*a);
+        cout << "Root: " << root << endl;
+    }
+    else {
+        cout << "No real roots." << endl;
+    }
+
+    return 0;
 }
- 
-        
           
               
      
