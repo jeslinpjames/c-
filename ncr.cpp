518448b3f6ab1445214b40a12fbf489c55b28c81
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 15:05:41 +0530

    Update ncr.cpp

--- ncr.cpp
+++ ncr.cpp
@@ -1,56 +1,27 @@
 #include<iostream>
 
 using namespace std;
-
-class cl
-
+int main()
 {
-
-        int main()
-
+        int n,r;
+        cout<<"Enter the Values of n and r:";
+        cin>>n;
+        cin>>r;
+        int a=fact(n);
+        int b=fact(r);
+        int c=fact(n-r);
+        int s=a/(b*c);
+        cout<<"Value of nCr="<<s;
+        return(0);
+}
+public: int fact(int n)
+{
+        int f=1;
+        while(n>0)
         {
-
-                int n,r;
-
-                cout<<"Enter the Values of n and r:";
-
-                cin>>n;
-
-                cin>>r;
-
-                cl f1;
-
-                int a=f1.fact(n);
-
-                int b=f1.fact(r);
-
-                int c=f1.fact(n-r);
-
-                int s=a/(b*c);
-
-                cout<<"Value of nCr="<<s;
-
-                return(0);
-
+                f=f*n;
+                n=n-1;
         }
-
-        public: int fact(int n)
-
-        {
-
-                int f=1;
-
-                while(n>0)
-
-                {
-
-                        f=f*n;
-
-                        n=n-1;
-
-                }
-
-                return (f);
-
-        }
+        return (f);
+}
 
