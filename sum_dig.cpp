a2ac35ae134014fac4c044f8eb0e71f8abd88a53
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 12:34:52 +0530

    Create sum_dig.cpp

---
+++ sum_dig.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+        int n,s=0;
+
+        cout<<"Enter the Number:";
+
+        cin>>n;
+
+        for(int i=1;i<=n;i++)
+
+                s=s+i;
+
+        cout<<"The Sum of Natural Numbers="<<s;
+
+        return(0);
+
+}
+
