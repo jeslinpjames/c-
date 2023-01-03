17bc8b448ce421397365347f7b3545a1ee664d06
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 15:57:05 +0530

    Create pascals.cpp

---
+++ pascals.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+int fact(int n)
+{
+        int f=1;
+        while(n>0)
+        {
+                f=f*n;
+                n=n-1;
+        }
+        return (f);
+}
+int ncr()
+{
+        int n,r;
+        cout<<"Enter the Values of n and r:";
+        cin>>n;
+        cin>>r;
+        int a=fact(n);
+        int b=fact(r);
+        int c=fact(n-r);
+        int s=a/(b*c);
+        cout<<"Value of nCr="<<s;
+        return(0);
+}
