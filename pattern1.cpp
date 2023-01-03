f560da47371e8b8c721c10980b8968c06a650e0c
jeslinpjames <114094231+jeslinpjames@users.noreply.github.com>
2023-01-03 12:39:02 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+        int l;
+
+        cout<<"Enter the Number of Rows:";
+
+        cin>>l;
+
+        for(int i=0;i<l;i++)
+
+        {
+
+                for(int j=0;j<=i;j++)
+
+                {
+
+                        cout<<"* ";
+
+                }
+
+                cout<<"\n";
+
+        }
+
+}
+
